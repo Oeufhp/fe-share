3b1f458ed10cb6c79d5cf3e6664f2b49f9a84a1a
Pitarn Harnpanichpun <pharn@sertiscorp.com>
2019-10-02 09:56:04 +0700

    minor add components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SketchPicker, TwitterPicker,  } from 'react-color'
+import { SketchPicker, TwitterPicker, BlockPicker, CirclePicker, HuePicker, AlphaPicker, MaterialPicker } from 'react-color'
 import './App.css';
 
 class App extends React.Component {
@@ -23,10 +23,17 @@ class App extends React.Component {
     let output = null
     if (this.state.getColorPicker) {
       output = (
-        <SketchPicker
-          color={ this.state.background }
-          onChangeComplete={(color) => this.handleChangeComplete(color)}
-        />
+				<SketchPicker
+					color={ this.state.background }
+					colors = {["#f44336", "#e91e63", "#9c27b0", "#673ab7", "#3f51b5", "#2196f3", "#03a9f4", "#00bcd4", "#009688", "#4caf50", "#8bc34a", "#cddc39", "#ffeb3b", "#ffc107", "#ff9800", "#ff5722", "#795548", "#607d8b"]}
+					onChange={(color) => this.handleChangeComplete(color)}
+				/>
+        // <CirclePicker
+        //   color={ this.state.background }
+				// 	circleSize={15}
+				// 	colors = {["#f44336", "#e91e63", "#9c27b0", "#673ab7", "#3f51b5", "#2196f3", "#03a9f4", "#00bcd4", "#009688", "#4caf50", "#8bc34a", "#cddc39", "#ffeb3b", "#ffc107", "#ff9800", "#ff5722", "#795548", "#607d8b"]}
+        //  onChange={(color) => this.handleChangeComplete(color)}
+        // />
       )
     }
     return output
