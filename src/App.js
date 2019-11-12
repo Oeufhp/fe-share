658ebdc4b44bf8520898fb6a7a337dac1f683f0d
Pitarn Harnpanichpun <pharn@sertiscorp.com>
2019-11-12 13:19:33 +0700

    add react-credit-cards

--- src/App.js
+++ src/App.js
@@ -1,67 +1,80 @@
 import React from 'react'
-import { Button, Form, Dropdown } from 'semantic-ui-react'
+import { Button, Form } from 'semantic-ui-react'
+import Cards from 'react-credit-cards'
 import 'semantic-ui-css/semantic.min.css'
+import 'react-credit-cards/es/styles-compiled.css'
 import './App.css'
 
 
-const localeOptions = [
-  {
-    key: 'Australia',
-    text: 'Australia',
-    value: 'au'
-  },
-  {
-    key: 'English(US)',
-    text: 'English(US)',
-    value: 'en_US'
-  },
-  {
-    key: 'China',
-    text: 'China',
-    value: 'zh_CN'
-  },
-  {
-    key: 'French',
-    text: 'French',
-    value: 'fr'
-  },
-]
-
 class App extends React.Component {
-
   state = {
-    locale: 'en_US'
+    cvc: '',
+    expiry: '',
+    focus: '',
+    name: '',
+    number: '',
   }
 
-
-  handleLocaleChange = (opt) => {
-    this.setState({
-      locale: opt.value
-    })
+  handleInputFocus = (e) => {
+    this.setState({ focus: e.target.name });
   }
 
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    this.setState({ [name]: value });
+  }
 
   render() {
     return (
       <div className="App">
+        <Cards
+          cvc={this.state.cvc}
+          expiry={this.state.expiry}
+          focused={this.state.focus}
+          name={this.state.name}
+          number={this.state.number}
+        />
         <Form>
-          <Dropdown
-            placeholder='localization'
-            fluid
-            selection
-            value={this.state.locale}
-            options={localeOptions}
-            onChange={this.handleLocaleChange}
-          />
           <Form.Field>
-            <label>First Name</label>
-            <input placeholder='First Name' />
+            <label>Name</label>
+            <input 
+              name='name'
+              placeholder='Name' 
+              onFocus={this.handleInputFocus}
+              onChange={this.handleInputChange}
+            />
           </Form.Field>
           <Form.Field>
-            <label>Last Name</label>
-            <input placeholder='Last Name' />
+            <label>Number</label>
+            <input
+              type="tel"
+              name='number'
+              maxLength={16}
+              placeholder='Number'
+              onFocus={this.handleInputFocus}
+              onChange={this.handleInputChange}
+            />
           </Form.Field>
-          <Button type='submit'>Submit</Button>
+          <Form.Field>
+            <label>expiry date</label>
+            <input
+              name='expiry'
+              maxLength={4}
+              placeholder='Expiry date'
+              onFocus={this.handleInputFocus}
+              onChange={this.handleInputChange}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>CVC</label>
+            <input 
+              name='cvc'
+              placeholder='CVC'
+              onFocus={this.handleInputFocus}
+              onChange={this.handleInputChange}
+            />
+          </Form.Field>
         </Form>
       </div>
     )
