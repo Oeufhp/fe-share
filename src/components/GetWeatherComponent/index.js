16f4d24bb1e398880abdf2e6c315000569e47de8
Oeuf <oeufhp@gmail.com>
2020-01-13 11:03:52 +0700

    add file and minor fix

--- src/components/GetWeatherComponent/index.js
+++ src/components/GetWeatherComponent/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import './style.scss'
 
 const GetWeatherComponent = () => {
 
@@ -7,7 +8,7 @@ const GetWeatherComponent = () => {
 
   useEffect(() => {
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${latLongObj.lat}&lon=${latLongObj.long}&appid=''`,
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latLongObj.lat}&lon=${latLongObj.long}&units=metric&appid=`,
       {
         method: 'GET',
       }
@@ -16,12 +17,11 @@ const GetWeatherComponent = () => {
     .then(res => {
       setWeatherData(res)
     })
-  }, [latLongObj])
+  }, [latLongObj.lat, latLongObj.long])
 
   const getCurrentWeather = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
-        console.log(position)
         setLatLongobj({ 
           lat: position.coords.latitude,
           long: position.coords.longitude 
@@ -31,21 +31,36 @@ const GetWeatherComponent = () => {
       alert('No gps')
     }
   }
+
+  console.log(weatherData)
+
   return (
     <div className='container'>
-
-      <button 
-        id='get-location-btn' 
-        onClick={getCurrentWeather()}
-      >
-        Get current location
-      </button>
+      <div className='btn-wrapper'>
+        <button 
+          id='get-location-btn' 
+          onClick={() => getCurrentWeather()}
+        >
+          Get current location
+        </button>
+        <button
+          id='reset-location-btn'
+          onClick={() => setLatLongobj({
+            lat: 0,
+            long: 0
+          })}
+        >
+          Reset location
+        </button>
+      </div>
+      
       {weatherData &&
         <div className='data-container'>
-          <div>{`Your current location is ${weatherData.name}`}</div>
+          <div>{`Current location: ${weatherData.name}`}</div>
           <div>{`Condition: ${weatherData.weather[0].description}`}</div>
-          <div>{`Temp: ${weatherData.main.temp/10} c`}</div>
-
+          <div>{`Temp: ${weatherData.main.temp} c`}</div>
+          <div>{`Pressure: ${weatherData.main.pressure} hPa`}</div>
+          <div>{`wind data: {speed: ${weatherData.wind.speed} m/s, degree: ${weatherData.wind.deg} degree} `}</div>
         </div>
       }
     </div>
